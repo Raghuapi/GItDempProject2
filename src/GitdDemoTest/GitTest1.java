1837a85e99bc77019f3cff26e02e941f93494695
raghavendra <raghavendra@SBLR-LT-0235.RGX.INT>
2022-05-21 00:46:20 +0530

    New changes

---
+++ src/GitdDemoTest/GitTest1.java
@@ -0,0 +1,21 @@
+package GitdDemoTest;
+
+public class GitTest1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String abc="Raghu";
+		
+		if(abc=="Raghu")
+		{
+			System.out.println("Name is Raghu");
+		}
+		else
+		{
+			System.out.println("Name is wrong");
+		}
+
+	}
+
+}
