e02e22b7ab081a9f00dff6ca1f861f934642b370
Raghuapi <86885131+Raghuapi@users.noreply.github.com>
2022-05-21 01:04:38 +0530

    Create test3.java

---
+++ src/GitdDemoTest/test3.java
@@ -0,0 +1,25 @@
+package GitdDemoTest;
+
+/*   Git hub repo details
+url  https://github.com/Raghuapi/GitDemoProjectV2.git
+pass : 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 */
+
+public class GitTest1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String abc="Raghu";
+		
+		if(abc=="Raghu")
+		{
+			System.out.println("Name is Raghu");
+		}
+		else
+		{
+			System.out.println("Name is wrong");
+		}
+
+	}
+
+}
